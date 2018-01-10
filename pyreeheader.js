06bbcda75da64b35b17b1fcb39499bc1c4141b9f
DrLuke <Lukas@Jackowski.de>
2018-01-10 22:23:16 +0100

    Version 1

--- pyreeheader.js
+++ pyreeheader.js
@@ -1,7 +1,17 @@
+var shaderProgram;
+
+var d = new Date();
+var tstart = d.getTime();
+var time = tstart;
+
+var mode = getRandomInt(0, 2);
+
+var fontArial;
+
 function initGL()
 {
     var canvas = document.getElementById("pyreeheader")
-    var gl = canvas.getContext("webgl")
+    var gl = canvas.getContext("webgl2")
 
     var vertices = [-1, 1, -1, -1, 1, -1, -1, 1, 1, 1, 1, -1];
 
@@ -11,59 +21,131 @@ function initGL()
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-    /* Step3: Create and compile Shader programs */
-
-    // Vertex shader source code
     var vertCode =
-        'attribute vec2 coordinates;' +
-        'void main(void) {' + ' gl_Position = vec4(coordinates,0.0, 1.0);' + '}';
+        '#version 300 es\n' +
+        'in vec2 coordinates;\n' +
+        '//out vec4 pos;\n' +
+        'void main(void) {' + ' gl_Position = vec4(coordinates, 0.0, 1.0);' + '}';
 
-    //Create a vertex shader object
     var vertShader = gl.createShader(gl.VERTEX_SHADER);
 
     gl.shaderSource(vertShader, vertCode);
     gl.compileShader(vertShader);
 
-    var fragCode = 'void main(void) {' + 'gl_FragColor = vec4(0.0, 0.3, 0.0, 1.);' + '}';
-    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+    console.log('Shader compiler log: ' + gl.getShaderInfoLog(vertShader));
 
+    var fragScript = document.getElementById("fshader");
+    var fragCode;
+    if(fragScript.type == "x-shader/x-fragment")
+    {
+        fragCode = fragScript.text;
+    }
+    else
+    {
+        fragCode = 'precision highp float;\n' +
+            'uniform float iTime;\n' +
+            'void main(void) {' + 'gl_FragColor = vec4(sin(iTime), 0.3, 0.0, 1.);' + '}';
+    }
+
+    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragShader, fragCode);
     gl.compileShader(fragShader);
 
-    var shaderProgram = gl.createProgram();
+    var compiled = gl.getShaderParameter(fragShader, gl.COMPILE_STATUS);
+    if(compiled == false)
+    {
+        console.log("FRAGMENT SHADER COMPILE ERROR:");
+        console.log('Shader compiler log: ' + gl.getShaderInfoLog(fragShader));
+
+        //return;
+    }
+
+    shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertShader);
     gl.attachShader(shaderProgram, fragShader);
-
     gl.linkProgram(shaderProgram);
-
     gl.useProgram(shaderProgram);
-
-    /* Step 4: Associate the shader programs to buffer objects */
-
-    //Bind vertex buffer object
     gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
 
-    //Get the attribute location
-    var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+    console.log("link: " + gl.getProgramInfoLog(shaderProgram));
 
-    //point an attribute to the currently bound VBO
+    //var coordAttribLoc = gl.getAttribLocation(shaderProgram, "coordinates");
+
+    var vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+
+    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(0);
+
+    /*var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+    console.log(coord);
     gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(coord);*/
 
-    //Enable the attribute
-    gl.enableVertexAttribArray(coord);
+    tstart = d.getTime()  - Math.random()*100000.;
 
+    // Set up texture
+    /*gl.enable(gl.TEXTURE_2D);
+    fontArial = gl.createTexture();
+    var arialImg = new Image();
+    arialImg.onload = function() {fontLoaded(arialImg, fontArial)};
+    arialImg.src = "foo.jpg";*/
 
     requestAnimationFrame(tickGL);
 }
 
+/*function fontLoaded(image, texture)
+{
+    var canvas = document.getElementById("pyreeheader")
+    var gl = canvas.getContext("webgl")
+
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}*/
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 function tickGL()
 {
     var canvas = document.getElementById("pyreeheader");
-    var gl = canvas.getContext("webgl");
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
 
-    // TODO: Shader logic
+    var gl = canvas.getContext("webgl2");
 
-    gl.viewport(0,0,canvas.width,canvas.height);
+    d = new Date();
+    time = (d.getTime() - tstart)/1000.;    // Calculate time since gl init
+
+    // Set uniforms
+    var loc = gl.getUniformLocation(shaderProgram, "iTime");
+    if(loc != -1)
+    {
+        gl.uniform1f(loc, time);
+    }
+
+    loc = gl.getUniformLocation(shaderProgram, "res");
+    if(loc != -1)
+    {
+        gl.uniform2f(loc, canvas.width, canvas.height);
+    }
+
+    loc = gl.getUniformLocation(shaderProgram, "mode");
+    if(loc != -1)
+    {
+        gl.uniform1f(loc, mode);
+    }
+
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.5, 0.5, 0.5, 0.9);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
